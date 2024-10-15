c7c12572878888bb7af3d555ce498e8322f2f741
Aown-Aamir <aownaamirkips@gmail.com>
2024-10-15 12:29:45 +0500

    drafted

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,13 +6,13 @@ import { IoMailOutline } from "react-icons/io5";
 function Footer() {
   return (
     <div className="h-screen lg:h-[50vh] px-5 bg-slate-950 flex flex-col lg:flex-row justify-center items-center gap-36 text-center">
-      <div className=" flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
         <div className="flex flex-col gap-3 lg:gap-2 text-white">
-          <h1 className="font-bold text-7xl">Lagger's Lab</h1>
+          <h1 className="font-bold text-7xl">Lagger&apos;s Lab</h1>
           <p className="text-center lg:text-right text-lg">Web Developer</p>
         </div>
       </div>
-      <div className=" flex justify-center items-center">
+      <div className="flex justify-center items-center">
         <ul className="flex flex-col justify-center items-center lg:items-start gap-2">
           <li className="flex justify-center items-center gap-2">
             <span className="text-3xl text-white">
